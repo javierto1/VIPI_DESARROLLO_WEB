c1beae2aa8b12e6e29a6755f4f836f80fe05a203
javierto1 <javitovigliocco@hotmail.com>
2022-05-12 17:20:48 +0200

    Subir a GIT

---
+++ Version 3-8-2017/contact_meII.php
@@ -0,0 +1,37 @@
+<?php
+
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['phone'])     ||
+   empty($_POST['email']) 		||
+   empty($_POST['select'])    ||  
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+      //echo $_POST;
+	echo '<p class="alert alert-danger">No se puede enviar el mensaje mientras tenga campos vacios.</p>';
+	return false;
+   }
+	
+$name = $_POST['name'];
+//$empresa = $_POST['empresa'];
+$phone = $_POST['phone'];
+$email_address = $_POST['email'];
+$select = $_POST['select'];
+//$otro = $_POST['otro'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'vipi.desarrolloweb@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
+$subject = "Vipi Contacto de:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$contenido = "Usted recibe este mensaje desde su sitio WEB.\n\n"."Aqui se muestran los detalles:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nSelect: $select\n\nMessage:\n$message";
+$header = "From:vipi.desarrolloweb@gmail.com \nReply-To:".$_POST["email"]."\n";
+$header .= "Mime-Version: 1.0\n";
+$header .= "Content-Type: text/plain";
+if(mail($to, $subject, $contenido, $header)){
+echo '<p class="alert alert-info">Mensaje enviado. Pronto te contactaremos '.$name.' </p>';
+} else{        
+echo '<p class="alert alert-danger">No se puede entregar el mensaje, escribinos vipi.desarrolloweb@gmail.com.</p>';
+}
+
+?>
---
+++ scrum.site88.net/Version 1.0/contact_me.php
@@ -0,0 +1,35 @@
+<?php
+
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['phone'])     ||
+   empty($_POST['email']) 		||  
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+      //echo $_POST;
+	echo "No se puede enviar el mensaje mientras tenga campos vacios";
+	return false;
+   }
+	
+$name = $_POST['name'];
+//$empresa = $_POST['empresa'];
+$phone = $_POST['phone'];
+$email_address = $_POST['email'];
+//$select = $_POST['select'];
+//$otro = $_POST['otro'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'cpibetta@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
+$subject = "Vipi Contacto de:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$contenido = "Usted recibe este mensaje desde su sitio WEB.\n\n"."Aqui se muestran los detalles:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nSelect: $select\n\nMessage:\n$message";
+$header = "From: cpibetta@gmail.com\nReply-To:".$_POST["email"]."\n";
+$header .= "Mime-Version: 1.0\n";
+$header .= "Content-Type: text/plain";
+if(mail($to, $subject, $contenido, $header)){
+echo "mensaje enviado";
+} else{        
+echo "No se pudo enviar";}
+
+?>
